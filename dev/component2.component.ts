e390ac872c6693f02e1019909c8da3b822af27da
Anthony Winters <a.r.winters@nforce-it.nl>
2016-05-17 03:18:21 +0200

    Routing Lifecycle Hooks

--- dev/component2.component.ts
+++ dev/component2.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {Router} from 'angular2/router';
+import {CanDeactivate} from 'angular/router';
 
 @Component({
     template: `
@@ -7,12 +8,14 @@ import {Router} from 'angular2/router';
         <button (click)="onNavigate()">Take me to Component 1</button>
     `
 })
-export class Component2Component {
-
+export class Component2Component implements CanDeactivate {
     constructor(private _router: Router) {}
 
     onNavigate() {
         this._router.navigate(['Component1', {source: 'Component2'}]);
     }
 
+    routerCanDeactivate(nextInstruction:ComponentInstruction, prevInstruction:ComponentInstruction):any {
+        return alert("Sure?");
+    }
 }
